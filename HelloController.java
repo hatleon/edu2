6caa7146c83f8a51e75018de3e8f51934d862178
hatleon <309135619@qq.com>
2023-12-16 18:09:53 +0800

    first commit

---
+++ HelloController.java
@@ -0,0 +1,12 @@
+package com.example.edu2;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String Hello() {
+        return "hello";
+    }
+}
---
+++ src/test/java/com/example/edu2/HelloControllerTest.java
@@ -0,0 +1,40 @@
+package com.example.edu2;
+
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class HelloControllerTest {
+//
+//    @Test
+//    void sayHello() {
+//    }
+//}
+
+// HelloControllerTest.java
+//package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testSayHello() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Hello, World!"));
+    }
+}
+
+
