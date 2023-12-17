dc3db64cddf2178b8aaf39f9f8dba5984a39d8ec
hatleon <309135619@qq.com>
2023-12-17 20:01:22 +0800

    test13

--- src/main/java/com/example/edu2/HelloController.java
+++ src/main/java/com/example/edu2/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, World! test 12";
+        return "Hello, World! test 13";
     }
 }
